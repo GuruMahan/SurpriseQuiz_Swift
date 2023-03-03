b87c089fb81561ccee0d492e0d6f9b66c1ab0065
Guru Mahan <iosdeve2022@gmail.com>
2023-03-03 12:59:23 +0530

    Initial Commit

---
+++ SurpriseQuizXIB POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurpriseQuizXIB POC
+//
+//  Created by Guru Mahan on 03/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
